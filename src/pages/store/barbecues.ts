d5a0c36d36a113ba3df3755eabe5564f73335128
igorbrands <igorcantelmo@gmail.com>
2022-08-25 17:33:44 -0300

    feat: creates state storage

---
+++ src/pages/store/barbecues.ts
@@ -0,0 +1,63 @@
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+import { createId } from '../../utils/createId';
+
+type State = {
+	barbecues: Bbq[];
+	actions: UseBarbecuesStoreActions;
+};
+
+type Guests = {
+	id: string;
+	name: string;
+	payedAmount: number;
+	hasPayed: boolean;
+};
+
+export type Bbq = {
+	id: string;
+	title: string;
+	description: string;
+	date: Date;
+	noBeerValue: number;
+	withBeerValue: number;
+	totalPayedValue: number;
+	guests: Guests[];
+};
+
+type UseBarbecuesStoreActions = {
+	createsBbq: (bbq: Bbq) => void;
+	deleteBbq: (id: string) => void;
+};
+
+export const useBarbecuesStore = create(
+	persist<State>(
+		(set, get) => ({
+			barbecues: [],
+
+			actions: {
+				createsBbq: (bbq: Bbq) => {
+					set(state => {
+						const newSteakData = {
+							...bbq,
+							id: createId(),
+							peoples: [],
+							amountPayed: 0
+						};
+
+						state.barbecues = [...state.barbecues, newSteakData];
+					});
+				},
+				deleteBbq: (id: string) => {
+					set(state => ({
+						...state,
+						barbecues: state.barbecues.filter(bbq => bbq.id !== id)
+					}));
+				}
+			}
+		}),
+		{
+			name: 'useEventStore'
+		}
+	)
+);
